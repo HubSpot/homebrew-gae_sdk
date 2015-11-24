c13db5007534f73bc91806bf81aafd5f6351462f
Emil Sit <sit@emilsit.net>
2015-11-23 22:52:34 -0500

    gae-sdk: Update to 1.9.28
    Needed in particular for the ability to use OAuth to access remote api.

--- gae-sdk.rb
+++ gae-sdk.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class GaeSdk < Formula
   homepage 'https://developers.google.com/appengine/'
-  url 'https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.25.zip'
-  sha1 'bbc98eb51b30fdc9500d44ab722ddcff8643089f'
+  url 'https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.28.zip'
+  sha1 'e5947b8368f457f02c38e5f431f1992e8257c8e6'
 
   def patches
     [
